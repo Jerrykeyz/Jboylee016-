b97e158ad7f7cf37c35170ad6bbee30f07889e22
Jerrykeyz <ayomideogunnusi016@gmail.com>
2024-07-25 11:14:19 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,7 @@
-const bar = document.getElementById("bars")
-let me = document.getElementById("clicked")
-function Click(){
-    me.textContent  = "Thanks for hitting the button I really appreciate."
+let navBars = document.getElementById("navbar")
+const show = () => {
+  navBars.style.right = "0"
 }
-
-// Project-area
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
-  }
-  
-  function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-  }
-  
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-  }
-
-  console.dir(document);
+const hide = () => {
+  navBars.style.right = "-200px"
+}
